4cd08adb35188164ee7a083c8d9fdb780b9a55db
shakibul-islam-dev <ishakibul704@gmail.com>
2026-05-18 00:13:18 +0600

    Adding Motion,HeroUi,ReactIcon and Tostify

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,10 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Poppins } from "next/font/google";
 import "./globals.css";
-
+const poppins = Poppins({
+  weight: "500",
+  variable: "--font-poppins-sans",
+  subsets: ["latin"],
+});
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -18,11 +22,8 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
-      <body className="min-h-full flex flex-col">{children}</body>
+    <html lang="en" className={`${poppins.variable}  h-full antialiased`}>
+      <body className="">{children}</body>
     </html>
   );
 }
