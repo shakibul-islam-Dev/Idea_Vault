689dbce770c4fee20bf90f0176a1ef1b8969d421
shakibul-islam-dev <ishakibul704@gmail.com>
2026-05-22 22:41:40 +0600

    Adding url again

--- app/action.js
+++ app/action.js
@@ -1,6 +1,5 @@
 "use server";
 
-console.log("Action.js ফাইল লোড হয়েছে!");
 import { auth } from "@/lib/auth";
 import { revalidatePath } from "next/cache";
 import { headers } from "next/headers";
